5fc1b04972bff9162e96db79d9d61962d31c5bac
aiiaiiiyo <aiiaiiiyo@gmx.com>
2018-06-14 16:11:40 +0200

    lesson20

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,12 +4,34 @@ import Fish from "./Fish";
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import base from "../base";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
+  componentDidMount(){
+    const { params } = this.props.match;
+    //reinstate localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
   addFish = (fish) => {
     //1. take copy of existing state
     const fishes = {...this.state.fishes};
@@ -23,17 +45,49 @@ class App extends React.Component {
     //alert('loading sample');
     this.setState({ fishes: sampleFishes });
   };
+
+  addToOrder = (key) => {
+    //1 take a copy of state
+    const order = {...this.state.order};
+    //2 Either add to the order or update the number in an order
+    order[key] = order[key] + 1 || 1;
+    //3 call setstate to update our state object
+    this.setState({ order });
+  }
+
+  updatedFish = (key, updatedFish) => {
+    // take copy of current fish
+    const fishes = {...this.state.fishes};
+    // update that state
+    fishes[key] = updatedFish;
+    //set to state
+    this.setState({fishes});
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
           </ul>
         </div>
-        <Order/>
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+        {/* everythign in state as prop spread <Order {...this.state}/> */}
+        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
+          updatedFish={this.updatedFish}
+        />
       </div>
     )
   }
